c32e08f041be087dbd461085c9ca7bee9a1d1b07
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2016-04-13 13:39:23 +0200

    Allow sequences with groups of characters
    For answering [this StackOverflow](http://stackoverflow.com/q/36596389/50065) question.

--- alignment.py
+++ alignment.py
@@ -15,11 +15,13 @@ def zeros(shape):
 match_award      = 10
 mismatch_penalty = -5
 gap_penalty      = -5 # both for opening and extanding
+gap = '----' # should be as long as your group of characters
+space = '    ' # should be as long as your group of characters
 
 def match_score(alpha, beta):
     if alpha == beta:
         return match_award
-    elif alpha == '-' or beta == '-':
+    elif alpha == gap or beta == '-----':
         return gap_penalty
     else:
         return mismatch_penalty
@@ -31,35 +33,35 @@ def finalize(align1, align2):
     i,j = 0,0
     
     #calcuate identity, score and aligned sequeces
-    symbol = ''
+    symbol = []
     found = 0
     score = 0
     identity = 0
     for i in range(0,len(align1)):
         # if two AAs are the same, then output the letter
         if align1[i] == align2[i]:                
-            symbol = symbol + align1[i]
+            symbol.append(align1[i])
             identity = identity + 1
             score += match_score(align1[i], align2[i])
     
         # if they are not identical and none of them is gap
-        elif align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-': 
+        elif align1[i] != align2[i] and align1[i] != gap and align2[i] != gap:
             score += match_score(align1[i], align2[i])
-            symbol += ' '
+            symbol.append(space)
             found = 0
     
         #if one of them is a gap, output a space
-        elif align1[i] == '-' or align2[i] == '-':          
-            symbol += ' '
+        elif align1[i] == '-----' or align2[i] == '-----':
+            symbol.append('     ')
             score += gap_penalty
     
     identity = float(identity) / len(align1) * 100
     
     print 'Identity =', "%3.3f" % identity, 'percent'
     print 'Score =', score
-    print align1
-    print symbol
-    print align2
+    print ''.join(align1)
+    # print ''.join(symbol)
+    print ''.join(align2)
 
 
 def needle(seq1, seq2):
@@ -81,7 +83,7 @@ def needle(seq1, seq2):
             score[i][j] = max(match, delete, insert)
 
     # Traceback and compute the alignment 
-    align1, align2 = '', ''
+    align1, align2 = [], []
     i,j = m,n # start from the bottom right cell
     while i > 0 and j > 0: # end toching the top or the left edge
         score_current = score[i][j]
@@ -90,27 +92,27 @@ def needle(seq1, seq2):
         score_left = score[i-1][j]
 
         if score_current == score_diagonal + match_score(seq1[i-1], seq2[j-1]):
-            align1 += seq1[i-1]
-            align2 += seq2[j-1]
+            align1.append(seq1[i-1])
+            align2.append(seq2[j-1])
             i -= 1
             j -= 1
         elif score_current == score_left + gap_penalty:
-            align1 += seq1[i-1]
-            align2 += '-'
+            align1.append(seq1[i-1])
+            align2.append(gap)
             i -= 1
         elif score_current == score_up + gap_penalty:
-            align1 += '-'
-            align2 += seq2[j-1]
+            align1.append(gap)
+            align2.append(seq2[j-1])
             j -= 1
 
     # Finish tracing up to the top left cell
     while i > 0:
-        align1 += seq1[i-1]
-        align2 += '-'
+        align1.append(seq1[i-1])
+        align2.append('-----')
         i -= 1
     while j > 0:
-        align1 += '-'
-        align2 += seq2[j-1]
+        align1.append('-----')
+        align2.append(seq2[j-1])
         j -= 1
 
     finalize(align1, align2)
@@ -143,24 +145,36 @@ def water(seq1, seq2):
                 max_j = j
                 max_score = score[i][j];
     
-    align1, align2 = '', ''    # initial sequences
+    align1, align2 = [], []    # initial sequences
     
     i,j = max_i,max_j    # indices of path starting point
     
     #traceback, follow pointers
     while pointer[i][j] != 0:
         if pointer[i][j] == 3:
-            align1 += seq1[i-1]
-            align2 += seq2[j-1]
+            align1.append(seq1[i-1])
+            align2.append(seq2[j-1])
             i -= 1
             j -= 1
         elif pointer[i][j] == 2:
-            align1 += '-'
-            align2 += seq2[j-1]
+            align1.append(gap)
+            align2.append(seq2[j-1])
             j -= 1
         elif pointer[i][j] == 1:
-            align1 += seq1[i-1]
-            align2 += '-'
+            align1.append(seq1[i-1])
+            align2.append(gap)
             i -= 1
 
     finalize(align1, align2)
+
+if __name__ == '__main__':
+    seq1 = ['[A0]', '[C0]', '[A1]', '[B1]']
+    seq2 = ['[A0]', '[A1]', '[B1]', '[C1]']
+
+    print "Needleman-Wunsch"
+    needle(seq1, seq2)
+    print
+    print "Smith-Waterman"
+    water(seq1, seq2)
+    
+    
